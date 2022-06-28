6e292a6b47d3ac6f35073888f7e4f50b67a5021d
fazi1live <faziislive@gmail.com>
2022-06-28 22:52:15 +0500

    add

---
+++ javascript-lecture-no-2/js/index.js
@@ -0,0 +1,51 @@
+//Arguments are not avaialble in arrow function
+
+// function add(x,y){
+//     console.log(arguments);
+//     return (x+y);
+// }
+
+
+
+
+// let Result = function addNumber(num1, num2){
+//     let result;
+//     result = num1 + num2;
+//     return result;
+// }
+
+
+// let addNumber = (num1, num2) => {
+//     let result;
+//     result = num1 + num2;
+//     return result
+// }
+// console.log(addNumber(2,4));
+
+// (num1, num2) => (num1+num2)
+// console.log(addNumber(4,6));
+
+// Square A Number
+
+// let SquareANumber = (num) => {
+//     return num*num;
+// }
+
+// console.log(SquareANumber(9));
+
+
+// let TimesTable = (KisKaTable, KahanTak) => {
+//     for(
+//         let x=1;
+//         x<=KahanTak;
+//         x++
+//         ){
+//         console.log(`${KisKaTable} * ${x} = ${KisKaTable*x}`)
+//     }
+// }
+
+// TimesTable(2,12);
+
+
+
+
