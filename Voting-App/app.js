2477e7a18db640f1b0b4221521bd6ba8a0eedc5b
fazi1live <faziislive@gmail.com>
2022-08-01 04:39:20 +0500

    add

--- Voting-App/app.js
+++ Voting-App/app.js
@@ -1,55 +1,78 @@
-// const valuesCards = [
-//     { 
-//     image: '../img/image1.png', 
-//     title: 'title 1', 
-//     content: 'super content 1',
-//     },
-//     {
-//     image: '../img/image2.png', 
-//     title: 'title 2', 
-//     content: 'super content 2'
-//     },
-//     { 
-//     image: '../img/image-3.png', 
-//     title: 'title3', 
-//     content: 'blablablablbalbalbabla blablaba'
-//     },
-//    ]
-// valuesCards.map(card=> {
-//   var cardDiv = document.createElement('div');
-//   cardDiv.innerHTML = `
-//                 <div class="product-header">
-//                   <img src="${card.image}"/>
-//                 </div>
-//                 <div class="product-content">
-//                   <h4>${card.title}</h4>
-//                   <p>${card.content}</p>
-//                 </div> 
-//                 <button class="info-button">+ info</button>`
-//   document.getElementsByClassName('products-cards')[0].appendChild(cardDiv);
-// })
 
 let CreateCard = () => {
+  try {
+    let CardArray = [];
+    let LocalStorageData = JSON.parse(localStorage.getItem('card-data'));
     let Name = document.querySelector('.Name').value;
     let ImageUrl = document.querySelector('.ImageUrl').value;
+    let CardContainer = document.querySelector('.CardContainer');
     let CardObject = new Object();
     CardObject.Name = Name;
     CardObject.ImageUrl = ImageUrl;
     CardObject.Id = Math.floor(Math.random() * 1000);
     CardObject.Points = 0;
-    
-    
-    
 
-
-    let CardContainer = document.querySelector('.CardContainer');
-    CardContainer.innerHTML = `
-    <div class="card" style="width: 18rem;">
-    <img src="${CardObject.ImageUrl}" class="card-img-top" alt="...">
-    <div class="card-body">
-      <p>Name: ${CardObject.Name}</p>
-      <p>Points: ${CardObject.Points}</p>
-    </div>
+    if (CardObject.Name === '' || CardObject.ImageUrl === '') {
+      let TemporaryLocalStorage = JSON.parse(localStorage.getItem('card-data'));
+      TemporaryLocalStorage.forEach(element => {
+        CardContainer.innerHTML += `
+        <div class="col mx-3 my-3">
+        <div class="card" style="width: 18rem;">
+        <img src="${element.ImageUrl}" class="card-img-top" alt="...">
+        <div class="card-body">
+          <p>Name: ${element.Name}</p>
+          <p>Points: ${element.Points}</p>
+        </div>
+            </div>
         </div>
     `
+      })
+      return
+    }
+
+    if (LocalStorageData === null || LocalStorageData.length === 0) {
+      CardArray = [];
+      CardArray.push(CardObject);
+      localStorage.setItem('card-data', JSON.stringify(CardArray));
+      CardContainer.innerHTML = `
+    <div class="col mx-3 my-3">
+          <div class="card" style="width: 18rem;">
+          <img src="${element.ImageUrl}" class="card-img-top" alt="...">
+          <div class="card-body">
+            <p>Name: ${element.Name}</p>
+            <p>Points: ${element.Points}</p>
+          </div>
+              </div>
+          </div>
+    `
+      return
+    }
+
+    if (LocalStorageData.length !== 0) {
+      CardArray = [];
+      LocalStorageData.forEach(element => {
+        CardArray.push(element);
+      });
+      CardArray.push(CardObject);
+      console.log(CardArray);
+      localStorage.setItem('card-data', JSON.stringify(CardArray));
+      let TemporaryLocalStorage = JSON.parse(localStorage.getItem('card-data'));
+      TemporaryLocalStorage.forEach(element => {
+        CardContainer.innerHTML += `
+          <div class="col mx-3 my-3">
+          <div class="card" style="width: 18rem;">
+          <img src="${element.ImageUrl}" class="card-img-top" alt="...">
+          <div class="card-body">
+            <p>Name: ${element.Name}</p>
+            <p>Points: ${element.Points}</p>
+          </div>
+              </div>
+          </div>
+    `
+      })
+      return
+    }
+  } catch (error) {
+    console.log(error);
+  }
 }
