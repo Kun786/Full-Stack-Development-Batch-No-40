28f5c801f6a24fc5c16edd98aa67841dacab00ea
fazi1live <fazi1live@gmail.com>
2023-01-11 18:50:43 +0500

    add

--- e-commerce-application-updated/front-end/src/app/main-module/main-components/view-product/view-product.component.ts
+++ e-commerce-application-updated/front-end/src/app/main-module/main-components/view-product/view-product.component.ts
@@ -12,35 +12,36 @@ import { LocalStoragesmanagementService } from 'src/app/Shared/Services/local-st
   styleUrls: ['./view-product.component.css']
 })
 export class ViewProductComponent implements OnInit {
-  public productObject : ProductInterface | any = {};
-  public url : string | any = 'http://localhost:8888/';
-  public selectedSizes:any = [];
-  public selectedQuantity:Number | any = 0;
-  public stockQuantity:any;
+  public productObject: ProductInterface | any = {};
+  public url: string | any = 'http://localhost:8888/';
+  public selectedSizes: any = [];
+  public selectedQuantity: Number | any = 0;
+  public stockQuantity: any;
   public enumButtonText = BUTTONTEXT;
   public offCanvas = '';
+  public cart: any = [];
   constructor(
-    private readonly ActivatedRoute:ActivatedRoute,
-    private readonly ProductService:ProductService,
-    private readonly MessengerService:MessengerService,
-    private readonly ToastrService:ToastrService,
-    private readonly LocalStorageManagementService:LocalStoragesmanagementService
+    private readonly ActivatedRoute: ActivatedRoute,
+    private readonly ProductService: ProductService,
+    private readonly MessengerService: MessengerService,
+    private readonly ToastrService: ToastrService,
+    private readonly LocalStorageManagementService: LocalStoragesmanagementService
   ) { }
 
   ngOnInit(): void {
     this.getRouterParamatersandGetDataFromBackEnd();
   }
 
-  public getRouterParamatersandGetDataFromBackEnd(){
+  public getRouterParamatersandGetDataFromBackEnd() {
     // const paraId = this.ActivatedRoute.snapshot.params['Id'];
     const parameterizedId = this.ActivatedRoute.snapshot.paramMap.get('Id');
-    this.ProductService.GetProductById(parameterizedId).subscribe((response:any) => {
+    this.ProductService.GetProductById(parameterizedId).subscribe((response: any) => {
       this.productObject = response.Result;
       this.stockQuantity = this.productObject.quantity;
     })
   }
 
-  public getSizes(event:any) {
+  public getSizes(event: any) {
     if (event.target.checked) {
       this.selectedSizes.push(event.target.value);
     }
@@ -49,49 +50,128 @@ export class ViewProductComponent implements OnInit {
     }
   }
 
-  public decrementQuantity(){
-    if(this.selectedQuantity > 0){
+  public decrementQuantity() {
+    if (this.selectedQuantity > 0) {
       let getDataFromLocalStorage = this.LocalStorageManagementService.getCartFromLocalStorage();
-      if(getDataFromLocalStorage !== null){
+      if (getDataFromLocalStorage !== null) {
         getDataFromLocalStorage.selectedQuantity--;
         this.LocalStorageManagementService.addToCart(getDataFromLocalStorage);
       }
-      this.selectedQuantity --;
+      this.selectedQuantity--;
     }
   }
 
-  public incrementQuantity(){
-    if(this.selectedQuantity < this.stockQuantity){
-      this.selectedQuantity ++;
+  public incrementQuantity() {
+    if (this.selectedQuantity < this.stockQuantity) {
+      this.selectedQuantity++;
     }
   }
 
-  public addToCart(){
-    if(this.selectedSizes.length<=0){
+  //Chat GPT say refractor kia hai ye
+  // public addToCart(){
+  //   if(!this.selectedSizes.length){
+  //     this.ToastrService.error('Please Select the Size before you proceeed to cart')
+  //     return;
+  //   }
+  //   let processedCartArray = this.LocalStorageManagementService.getCartFromLocalStorage() || [];
+  //   let processedCartObject = {
+  //     productId: this.productObject._id,
+  //     imageUrl: this.productObject?.imageDetaisl[0]?.ImageUrl,
+  //     selectedSizes: this.selectedSizes,
+  //     selectedQuantity: this.selectedQuantity,
+  //     ...this.productObject
+  //   }
+  //   processedCartArray.push(processedCartObject);
+  //   this.LocalStorageManagementService.addToCart(processedCartArray);
+  // }
+  //Chat GPT say refractor kia hai ye
+
+  //Without Refgractor
+  public addToCart() {
+    if (this.selectedSizes.length <= 0) {
       this.ToastrService.error('Please Select the Size before you proceeed to cart')
-    }else{
-      this.offCanvas='offcanvas';
-      let { _id, category, color, companyName, description, price, productName, quantity} = this.productObject; 
+    } else {
+      this.offCanvas = 'offcanvas';
+      let { _id, category, color, companyName, description, price, productName, quantity } = this.productObject;
       let processedCartObject = {
-        productId:_id,
-        imageUrl:this.productObject?.imageDetaisl[0]?.ImageUrl,
-        selectedSizes:this.selectedSizes,
-        selectedQuantity:this.selectedQuantity,
+        productId: _id,
+        imageUrl: this.productObject?.imageDetaisl[0]?.ImageUrl,
+        selectedSizes: this.selectedSizes,
+        selectedQuantity: this.selectedQuantity,
         category, color, companyName, description, price, productName, quantity
       }
       let processedCartArray = [];
-      if(this.LocalStorageManagementService.getCartFromLocalStorage() !== null){
+      if (this.LocalStorageManagementService.getCartFromLocalStorage() !== null) {
         let valueAlreadyExists = this.LocalStorageManagementService.getCartFromLocalStorage();
-        valueAlreadyExists.forEach((element:any) => {
+        valueAlreadyExists.forEach((element: any) => {
           processedCartArray.push(element);
         });
       }
       processedCartArray.push(processedCartObject);
       this.LocalStorageManagementService.addToCart(processedCartArray);
     }
+    this.getCart();
+  }
+  //Without Refgractor
+
+  //Chatgpt ccode 
+  // decrementQuantityFromCanvas(id: any) {
+  //   const cart = this.LocalStorageManagementService.getCartFromLocalStorage();
+  //   const item = cart.find(item => item.productId === id);
+  //   if (item && item.quantity > item.selectedQuantity) {
+  //     item.selectedQuantity++;
+  //     this.LocalStorageManagementService.addToCart(cart);
+  //   }
+  //   this.getCart();
+  // }
+  //Chatgpt ccode 
+
+  decrementQuantityFromCanvas(id: any) {
+    const cartFromLocalStorage = this.LocalStorageManagementService.getCartFromLocalStorage();
+    cartFromLocalStorage.forEach((object: any) => {
+      if (object.productId === id && object.selectedQuantity > 0) {
+        object.selectedQuantity--;
+        this.LocalStorageManagementService.addToCart(cartFromLocalStorage)
+      }
+    });
+    this.getCart();
   }
 
-  public viewCart(){
+  incrementQuantityFromCanvas(id: any) {
+    // Phelay local storages maind say Array ley kar aao
+    const cartFromLocalStorage = this.LocalStorageManagementService.getCartFromLocalStorage();
 
+    //Bhai abb loop karo  aur phir id ke according  woobject ley  kar  aoo  aur  uss  ke quanitity  bas ++ kardo
+    cartFromLocalStorage.forEach((object: any) => {
+      if (object.productId === id && object.quantity > object.selectedQuantity) {
+        object.selectedQuantity++;
+        this.LocalStorageManagementService.addToCart(cartFromLocalStorage)
+      }
+    });
+    this.getCart();
   }
+
+
+  public viewCart() {
+    this.getCart();
+  }
+
+  public getCart() {
+    this.cart = this.LocalStorageManagementService.getCartFromLocalStorage();
+    /*
+    [
+      0:{},
+      1:{},
+      2:{}
+    ]
+    */
+  }
+
+  deleteItem(id:any){
+    const cartFromLocalStorage = this.LocalStorageManagementService.getCartFromLocalStorage(); // sari array   from  local storage
+    const updatedObject = cartFromLocalStorage.filter((object:any) => object.productId  !=  id); // filter
+    this.LocalStorageManagementService.addToCart(updatedObject);
+    this.getCart();
+  }
+
 }
